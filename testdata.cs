e05e0fcefe2565f6b167ef6b51cc2679c4fd3793
Pranathicheripelly <147033171+Pranathicheripelly@users.noreply.github.com>
2023-10-05 14:43:57 +0530

    Add files via upload

---
+++ testdata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swapnumbers
+{
+    internal class testdata
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z;
+            Console.WriteLine("enter first number:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter second number:");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter third number:");
+            z = Convert.ToDouble(Console.ReadLine());
+            double result1 = (x + y) * z;
+            double result2 = (x * y) + (y * z);
+            Console.WriteLine($"result of specified numbers {x},{y},{z},(x+y).z is {result1} and x.y+y.z is {result2} ");
+            Console.ReadLine();
+
+        }
+    }
+}
